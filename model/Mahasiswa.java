3324ff54fc197611725c6b7df89fa6e1830d97a9
rifqi <rifqihidayatmuhammad@gmail.com>
2024-03-31 13:59:24 +0800

    Memanggil Banyak nethod dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa {
+    public static void tampilanInfo() {
+        System.out.println();
+        System.out.println("== Data Maha Siswa ===============================");
+        System.out.println("NPM : 2210010126");
+        System.out.println("Nama : Muhammad Rifqi Hidayat");
+        System.out.println("=============================================");
+    }
+}
